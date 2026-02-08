08b4b4da0b9b03984a044f8c08013934513491b6
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-02-09 00:36:08 +0600

    added rtk query cache update tag in cart api

--- src/redux/api/cartApi.js
+++ src/redux/api/cartApi.js
@@ -3,29 +3,37 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const cartApi = createApi({
   reducerPath: "cartApi",
   baseQuery: fetchBaseQuery({ baseUrl: "/api", credentials: "include" }),
+  tagTypes: ["Cart"],
   endpoints: (builder) => ({
     getCart: builder.query({
       query: () => "/cart",
+      providesTags: ["Cart"],
     }),
+
     addToCart: builder.mutation({
       query: ({ productId, qty = 1 }) => ({
         url: "/cart",
         method: "POST",
         body: { productId, qty },
       }),
+      invalidatesTags: ["Cart"],
     }),
+
     updateQty: builder.mutation({
       query: ({ productId, qty }) => ({
         url: "/cart",
         method: "PATCH",
         body: { productId, qty },
       }),
+      invalidatesTags: ["Cart"],
     }),
+
     removeCartItem: builder.mutation({
       query: (productId) => ({
         url: `/cart?productId=${productId}`,
         method: "DELETE",
       }),
+      invalidatesTags: ["Cart"],
     }),
   }),
 });
