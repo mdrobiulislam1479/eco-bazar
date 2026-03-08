7e8e81e802887bed066fcb70cbf6b20b1d011e01
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-03-08 22:38:26 +0600

    added metadata in root level

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,15 @@ import PopularProducts from "@/components/home/PopularProducts";
 import SummerSaleBanner from "@/components/home/SummerSaleBanner";
 import Testimonials from "@/components/shared/Testimonials";
 
+export const metadata = {
+  title: "Eco Bazar | Home",
+  description:
+    "Discover the latest deals, featured products, and sustainable living tips at Eco Bazar. Shop now and make your lifestyle greener!",
+  alternates: {
+    canonical: "https://eco-bazar-one.vercel.app",
+  },
+};
+
 const Home = () => {
   return (
     <div>
