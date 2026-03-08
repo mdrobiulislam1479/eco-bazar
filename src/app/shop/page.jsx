a82f63f0171e99ae287dcc727d3ff896c1171f6a
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-03-08 22:45:40 +0600

    added metadata in shop page

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,5 +1,39 @@
 import ShopClient from "@/components/shop/ShopClient";
 
+export const metadata = {
+  title: "Shop Organic Groceries | Eco Bazar",
+  description:
+    "Explore Eco Bazar's shop for fresh organic groceries, eco-friendly products, and sustainable lifestyle essentials.",
+
+  keywords: [
+    "organic grocery",
+    "eco friendly shop",
+    "online grocery store",
+    "sustainable products",
+  ],
+
+  alternates: {
+    canonical: "https://eco-bazar-one.vercel.app/shop",
+  },
+
+  openGraph: {
+    title: "Shop Organic Groceries | Eco Bazar",
+    description:
+      "Browse organic groceries and eco-friendly products at Eco Bazar.",
+    url: "https://eco-bazar-one.vercel.app/shop",
+    siteName: "Eco Bazar",
+    images: [
+      {
+        url: "https://i.ibb.co/GmP29W4/image.png",
+        width: 1200,
+        height: 630,
+        alt: "Eco Bazar Shop",
+      },
+    ],
+    type: "website",
+  },
+};
+
 const Shop = () => {
   return (
     <div>
